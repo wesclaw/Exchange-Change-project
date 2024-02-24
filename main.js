b9bde58a28d6ae3d0bd987fc9462d45bdf8585f1
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-02-24 15:54:05 +0100

    added 10$

--- main.js
+++ main.js
@@ -300,6 +300,32 @@ function giveCoins(getText) {
     }
   } else if (getText === '2 tens') {
     ///shoot out 2 tens
+    cashmoneyout.classList.remove('addToBend')
+    cashmoneyout.classList.add('addThisForMoneyOut')
+    for (let i = 0; i <= 1; i++) {
+      buttons.forEach((btn)=>{
+        btn.disabled = true
+      })
+      setTimeout(() => { 
+        const cash = document.createElement('img');
+        cash.src = 'images/ten-dollar-bill.png';
+        cash.classList.add('cash');
+        atm.append(cash);
+        if (i === 1) { 
+          setTimeout(()=>{ 
+            cashmoneyout.classList.remove('addThisForMoneyOut')
+            cashmoneyout.classList.add('addToBend')
+            screen.innerHTML = `<h1>Thank you!</h1>`;
+            setTimeout(()=>{
+              screen.innerHTML = ``; 
+              buttons.forEach((btn)=>{
+                btn.disabled = false
+              })
+            },2000)
+          },2000)
+        }
+      }, i * 400); 
+    }
   } else if (getText === '4 fives') {
     ///shoot out 4 fives
     cashmoneyout.classList.remove('addToBend')
@@ -358,6 +384,32 @@ function giveCoins(getText) {
     }
   } else if (getText === '5 tens') {
     ///shoot out 5 tens
+    cashmoneyout.classList.remove('addToBend')
+    cashmoneyout.classList.add('addThisForMoneyOut')
+    for (let i = 0; i <= 4; i++) {
+      buttons.forEach((btn)=>{
+        btn.disabled = true
+      })
+      setTimeout(() => { 
+        const cash = document.createElement('img');
+        cash.src = 'images/ten-dollar-bill.png';
+        cash.classList.add('cash');
+        atm.append(cash);
+        if (i === 4) { 
+          setTimeout(()=>{ 
+            cashmoneyout.classList.remove('addThisForMoneyOut')
+            cashmoneyout.classList.add('addToBend')
+            screen.innerHTML = `<h1>Thank you!</h1>`;
+            setTimeout(()=>{
+              screen.innerHTML = ``; 
+              buttons.forEach((btn)=>{
+                btn.disabled = false
+              })
+            },2000)
+          },2000)
+        }
+      }, i * 400); 
+    }
   } else if (getText === '10 fives') {
     ///shoot out 10 fives
     cashmoneyout.classList.remove('addToBend')
@@ -422,6 +474,32 @@ function giveCoins(getText) {
     ///shoot out 5 twenties
   }else if(getText==='10 tens'){
     ///shoot out 10 tens
+    cashmoneyout.classList.remove('addToBend')
+    cashmoneyout.classList.add('addThisForMoneyOut')
+    for (let i = 0; i <= 9; i++) {
+      buttons.forEach((btn)=>{
+        btn.disabled = true
+      })
+      setTimeout(() => { 
+        const cash = document.createElement('img');
+        cash.src = 'images/ten-dollar-bill.png';
+        cash.classList.add('cash');
+        atm.append(cash);
+        if (i === 9) { 
+          setTimeout(()=>{ 
+            cashmoneyout.classList.remove('addThisForMoneyOut')
+            cashmoneyout.classList.add('addToBend')
+            screen.innerHTML = `<h1>Thank you!</h1>`;
+            setTimeout(()=>{
+              screen.innerHTML = ``; 
+              buttons.forEach((btn)=>{
+                btn.disabled = false
+              })
+            },2000)
+          },2000)
+        }
+      }, i * 400); 
+    }
   }else if(getText==='20 fives') {
     ///shoot out 20 fives
     cashmoneyout.classList.remove('addToBend')
